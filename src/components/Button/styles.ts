f2a8663dce89b744e7740bea26dbc3610f3b8588
Germano Lira <dolan.3@hotmail.com>
2021-11-02 20:13:39 -0300

    :art: finish details screen

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components/native';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { RectButton, RectButtonProps } from 'react-native-gesture-handler';
+
+interface ButtonProps extends RectButtonProps {
+  color: string;
+}
+
+export const Container = styled(RectButton) <ButtonProps>`
+  width: 100%;
+  background-color: ${({ color, theme }) => color ? color : theme.colors.main};
+
+  padding: 19px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.primary_400};
+  font-size: ${RFValue(15)}px;
+  color: ${({ theme }) => theme.colors.shape};
+`;
