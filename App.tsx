f456fddc039aa1f9933ed4c1a390fc97b22b59f5
Germano Lira <dolan.3@hotmail.com>
2021-11-13 23:34:26 -0300

    :art: :evergreen_tree: create schedule page style

--- App.tsx
+++ App.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import AppLoading from 'expo-app-loading';
 import { ThemeProvider } from 'styled-components';
 
-import { CarDetails } from './src/screens/CarDetails';
-
 import theme from './src/styles/theme';
 
 import {
@@ -18,6 +16,7 @@ import {
   Archivo_600SemiBold,
 } from '@expo-google-fonts/archivo';
 
+import { Scheduling } from './src/screens/Scheduling';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -34,7 +33,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <CarDetails />
+      <Scheduling />
     </ThemeProvider>
   )
 }
