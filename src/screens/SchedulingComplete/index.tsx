6673288545f1fe8ea4b0eba8da9386d4f5f5bbc0
Germano Lira <dolan.3@hotmail.com>
2021-12-01 01:22:14 -0300

    :art: create scheduling complete screen

--- src/screens/SchedulingComplete/index.tsx
+++ src/screens/SchedulingComplete/index.tsx
@@ -4,19 +4,22 @@ import { useWindowDimensions } from 'react-native';
 // { Dimensions } só é usado em lugares que não consegue usar hooks
 // Hooks só podem ser usados em componentes React, ou seja, dentro de uma função
 
-import LogoSvg from '../../../assets/logo_background_gray.svg';
-import DoneSvg from '../../../assets/done.svg';
+import LogoSvg from '../../assets/logo_background_gray.svg';
+import DoneSvg from '../../assets/done.svg';
 
 import {
   Container,
   Content,
   Title,
   Message,
+  Footer,
 } from './styles';
+import { ConfirmButton } from '../../components/ConfirmButton';
 
-const { width } = useWindowDimensions();
 
 export function SchedulingComplete() {
+  const { width } = useWindowDimensions();
+
   return (
     <Container>
       <LogoSvg width={width} />
@@ -31,6 +34,12 @@ export function SchedulingComplete() {
           pegar o seu automóvel.
         </Message>
       </Content>
+
+      <Footer>
+        <ConfirmButton
+          title="Ok"
+        />
+      </Footer>
     </Container>
   );
 }
